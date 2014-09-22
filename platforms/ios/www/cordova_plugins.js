976b0093ca15394651bb0808ab69a358c7fe32ea
Koray Balci <koray.balci@ccyazilim.com>
2014-09-22 13:08:14 +0300

    Updated fb plugin from github.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.ccsoft.plugin.CordovaFacebook/www/CordovaFacebook.js",
-        "id": "com.ccsoft.plugin.CordovaFacebook.CordovaFacebook",
-        "merges": [
-            "CC"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
         "id": "org.apache.cordova.console.console",
         "clobbers": [
@@ -20,13 +13,20 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/com.ccsoft.plugin.CordovaFacebook/www/CordovaFacebook.js",
+        "id": "com.ccsoft.plugin.CordovaFacebook.CordovaFacebook",
+        "merges": [
+            "CC"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.ccsoft.plugin.CordovaFacebook": "1.0.1",
-    "org.apache.cordova.console": "0.2.5"
+    "org.apache.cordova.console": "0.2.5",
+    "com.ccsoft.plugin.CordovaFacebook": "1.0.2"
 }
 // BOTTOM OF METADATA
 });
